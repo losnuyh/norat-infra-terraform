c6bf88c6c5784838ffee6d01fdf2fb869fb7f23e
sol <solhyungusthf@gmail.com>
2024-02-11 14:21:22 +0900

    사용하지 않는 환경변수 정리

--- environments/dev/3_application/main.tf
+++ environments/dev/3_application/main.tf
@@ -6,8 +6,6 @@ module "application" {
   vpc_id = var.vpc_id
   public_subnet_ids = var.public_subnet_ids
   private_subnet_ids = var.private_subnet_ids
-  instance_type = var.instance_type
-  application_instance_ami = var.application_instance_ami
   bastion_sg_id = var.bastion_sg_id
   api_lambda_sg_id = var.api_lambda_sg_id
   default_api_lambda_deploy = var.default_api_lambda_deploy
