4b807d7f66a11009128680e8b14e0f5d1f7cf8f8
COCOLMAN <iam@hyunsol.com>
2024-02-17 19:38:27 +0900

    common으로 이동

--- modules/3_application/app_parameter/parameter_store.tf
+++ modules/3_application/app_parameter/parameter_store.tf
@@ -5,71 +5,7 @@ locals {
   common_prefix = "/${var.project}/COMMON/${var.env}"
 }
 
-resource "aws_ssm_parameter" "write_db_user_name_for_main" {
-  name = "${local.main_prefix}/WRITE_DB_USER_NAME"
-  type = "String"
-  value = var.write_db_user_name
-}
-
-resource "aws_ssm_parameter" "write_db_user_password_for_main" {
-  name = "${local.main_prefix}/WRITE_DB_USER_PASSWORD"
-  type = "SecureString"
-  value = var.write_db_user_password
-}
-
-resource "aws_ssm_parameter" "write_db_host_for_main" {
-  name = "${local.main_prefix}/WRITE_DB_HOST"
-  type = "String"
-  value = var.write_db_host
-}
-
-resource "aws_ssm_parameter" "read_db_user_name_for_main" {
-  name = "${local.main_prefix}/READ_DB_USER_NAME"
-  type = "String"
-  value = var.read_db_user_name
-}
-
-resource "aws_ssm_parameter" "read_db_user_password_for_main" {
-  name = "${local.main_prefix}/READ_DB_USER_PASSWORD"
-  type = "SecureString"
-  value = var.read_db_user_password
-}
-
-resource "aws_ssm_parameter" "read_db_host_for_main" {
-  name = "${local.main_prefix}/READ_DB_HOST"
-  type = "String"
-  value = var.read_db_host
-}
-
-resource "aws_ssm_parameter" "db_port_for_main" {
-  name = "${local.main_prefix}/DB_PORT"
-  type = "String"
-  value = var.db_port
-}
-
-resource "aws_ssm_parameter" "database_name_for_main" {
-  name = "${local.main_prefix}/DATABASE_NAME"
-  type = "String"
-  value = var.database_name
-}
-
-resource "aws_ssm_parameter" "cool_sms_api_key_for_main" {
-  name = "${local.main_prefix}/COOL_SMS_API_KEY"
-  type = "SecureString"
-  value = var.cool_sms_api_key
-}
-
-resource "aws_ssm_parameter" "cool_sms_secret_key_for_main" {
-  name = "${local.main_prefix}/COOL_SMS_SECRET_KEY"
-  type = "SecureString"
-  value = var.cool_sms_api_secret_key
-}
-
-resource "aws_ssm_parameter" "cool_sms_send_phone_number_for_main" {
-  name = "${local.main_prefix}/COOL_SMS_SEND_PHONE_NUMBER"
-  type = "String"
-  value = var.cool_sms_send_phone_number
-}
+# main
 
 resource "aws_ssm_parameter" "neis_key_for_main" {
   name = "${local.main_prefix}/NEIS_KEY"
@@ -77,26 +13,93 @@ resource "aws_ssm_parameter" "neis_key_for_main" {
   value = var.neis_key
 }
 
-resource "aws_ssm_parameter" "port_one_key_for_main" {
-  name = "${local.main_prefix}/PORT_ONE_KEY"
-  type = "SecureString"
-  value = var.port_one_key
-}
-
-resource "aws_ssm_parameter" "port_one_secret_for_main" {
-  name = "${local.main_prefix}/PORT_ONE_SECRET"
-  type = "SecureString"
-  value = var.port_one_secret
-}
-
-resource "aws_ssm_parameter" "user_upload_bucket_name_for_main" {
-  name = "${local.main_prefix}/USER_UPLOAD_S3_BUCKET_NAME"
-  type = "String"
-  value = var.user_upload_bucket_name
-}
+# common
 
 resource "aws_ssm_parameter" "jwt_signing_private_key_for_common" {
   name = "${local.common_prefix}/JWT_SIGNING_PRIVATE_KEY"
   type = "SecureString"
   value = var.jwt_signing_private_key
+}
+
+resource "aws_ssm_parameter" "write_db_user_name_for_common" {
+  name = "${local.common_prefix}/WRITE_DB_USER_NAME"
+  type = "String"
+  value = var.write_db_user_name
+}
+
+resource "aws_ssm_parameter" "write_db_user_password_for_common" {
+  name = "${local.common_prefix}/WRITE_DB_USER_PASSWORD"
+  type = "SecureString"
+  value = var.write_db_user_password
+}
+
+resource "aws_ssm_parameter" "write_db_host_for_common" {
+  name = "${local.common_prefix}/WRITE_DB_HOST"
+  type = "String"
+  value = var.write_db_host
+}
+
+resource "aws_ssm_parameter" "read_db_user_name_for_common" {
+  name = "${local.common_prefix}/READ_DB_USER_NAME"
+  type = "String"
+  value = var.read_db_user_name
+}
+
+resource "aws_ssm_parameter" "read_db_user_password_for_common" {
+  name = "${local.common_prefix}/READ_DB_USER_PASSWORD"
+  type = "SecureString"
+  value = var.read_db_user_password
+}
+
+resource "aws_ssm_parameter" "read_db_host_for_common" {
+  name = "${local.common_prefix}/READ_DB_HOST"
+  type = "String"
+  value = var.read_db_host
+}
+
+resource "aws_ssm_parameter" "db_port_for_common" {
+  name = "${local.common_prefix}/DB_PORT"
+  type = "String"
+  value = var.db_port
+}
+
+resource "aws_ssm_parameter" "database_name_for_common" {
+  name = "${local.common_prefix}/DATABASE_NAME"
+  type = "String"
+  value = var.database_name
+}
+
+resource "aws_ssm_parameter" "cool_sms_api_key_for_common" {
+  name = "${local.common_prefix}/COOL_SMS_API_KEY"
+  type = "SecureString"
+  value = var.cool_sms_api_key
+}
+
+resource "aws_ssm_parameter" "cool_sms_secret_key_for_common" {
+  name = "${local.common_prefix}/COOL_SMS_SECRET_KEY"
+  type = "SecureString"
+  value = var.cool_sms_api_secret_key
+}
+
+resource "aws_ssm_parameter" "cool_sms_send_phone_number_for_common" {
+  name = "${local.common_prefix}/COOL_SMS_SEND_PHONE_NUMBER"
+  type = "String"
+  value = var.cool_sms_send_phone_number
+}
+resource "aws_ssm_parameter" "port_one_key_for_common" {
+  name = "${local.common_prefix}/PORT_ONE_KEY"
+  type = "SecureString"
+  value = var.port_one_key
+}
+
+resource "aws_ssm_parameter" "port_one_secret_for_common" {
+  name = "${local.common_prefix}/PORT_ONE_SECRET"
+  type = "SecureString"
+  value = var.port_one_secret
+}
+
+resource "aws_ssm_parameter" "user_upload_bucket_name_for_common" {
+  name = "${local.common_prefix}/USER_UPLOAD_S3_BUCKET_NAME"
+  type = "String"
+  value = var.user_upload_bucket_name
 }
